0c06025527faacab5dd6c4d7064a71a1118be2c4
Chester Tan <rey.tan@obf.ateneo.edu>
2023-09-25 21:57:56 +0800

    added vggunet and unet permutations

---
+++ pyunet/lib/wnet/wnet_unet_vggunet.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from lib.unet import UNet
+from lib.vgg_unet import VGGUNet
+    
+class WNetUNetVGGUNet(nn.Module):
+    def __init__(
+        self, in_channels=3, out_channels=3, k=1
+    ):
+        super(WNetUNetVGGUNet, self).__init__()
+
+        self.enc = UNet(in_channels, k)
+        self.dec = VGGUNet(k, out_channels)
+        self.softmax2d = nn.Softmax2d()
+
+    def forward(self, x, mode='dec'):
+
+        encoded = self.enc(x)
+        encoded = self.softmax2d(encoded)
+        if mode == 'enc':
+            return encoded
+
+        decoded = self.dec(encoded)
+        
+        if mode == 'dec':
+            return decoded
+        
+        return encoded, decoded
+
